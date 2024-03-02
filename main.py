94c9cce92d56d9f01762518a75ca8d6aa07c968a
TimurMalkc <timurmlkc@gmail.com>
2024-03-02 20:48:36 +0300

    added storage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import ctypes
+import os
 
 window = tk.Tk()
 window.title("Planner")
@@ -9,11 +10,38 @@ frameList.pack(side=tk.BOTTOM, fill = tk.BOTH, expand=True)
 frameOther = tk.Frame(window, background="blue")
 frameOther.pack(side=tk.TOP, fill=tk.X, expand=False)
 
-def openWindow():
+path = "C:\\Users\\timur\\PycharmProjects\\NotesAppStorage"
+os.chdir(path)
+
+def read_notes(file_path):
+    with open(file_path, "r") as f:
+        print(f.read())
+
+def open_notes(noteName):
     newWindow = tk.Toplevel(window)
-    newWindow.title("Not1")
+    newWindow.title(noteName)
     newWindow.state("zoomed")
-    tk.Label(newWindow, text="aa", background="red",height=100, width=40).pack()
+    file_path = f"{path}\\{noteName}"
+
+    def saveNote():
+        newText = notesText.get("1.0", "end-1c")
+        with open(file_path, "w") as f:
+            f.write(newText)
+            f.close()
+
+    notesText = tk.Text(newWindow, background="red",height=20, width=40)
+    notesText.pack()
+    saveButton = tk.Button(newWindow, text="Save", background="blue", width=10, height=5,
+                    command= saveNote,
+                    font=("Comic Sans", 10),
+                    fg="#EEF5FF", bg="#B4D4FF",
+                    activeforeground="#EEF5FF", activebackground="#B4D4FF",padx=4).pack()
+
+    with open(file_path, "r") as f:
+        notesText.insert(tk.END, f.read())
+
+
+
 
 def addNote():
     if(nameEntry.get() != ""):
@@ -22,9 +50,11 @@ def addNote():
 
 def onselect(evt):
     w = evt.widget
-    index = int(w.curselection()[0])
-    value = w.get(index)
-    print('You selected item %d: "%s"' % (index, value))
+    if(len(w.curselection()) != 0):
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        print('You selected item %d: "%s"' % (index, value))
+        open_notes(value)
 
 noteButton = tk.Button(frameOther, text="New Note", background="blue", width=10, height=5,
                     command= addNote,
@@ -32,6 +62,7 @@ noteButton = tk.Button(frameOther, text="New Note", background="blue", width=10,
                     fg="#EEF5FF", bg="#B4D4FF",
                     activeforeground="#EEF5FF", activebackground="#B4D4FF",padx=4)
 noteButton.grid(row=0, column=0, sticky = tk.W)
+
 
 nameEntry = tk.Entry(frameOther, font=("Ariel",35))
 nameEntry.grid(row=0, column=1, sticky = tk.W)
@@ -42,9 +73,18 @@ scroller.pack(side=tk.RIGHT,fill=tk.Y )
 
 notesList = tk.Listbox(frameList, yscrollcommand = scroller.set, font=("Comic Sans", 20), bg="#55adad", fg="white")
 notesList.pack(side=tk.TOP, fill = tk.BOTH, expand=True )
-notesList.bind("<<ListboxSelect>>", onselect)
+notesList.bind("<Double-Button>", onselect)
 
 scroller.config( command = notesList.yview )
+
+for file in os.listdir():
+    if file.endswith(".txt"):
+        file_path = f"{path}\\{file}"
+        notesList.insert(tk.END, file)
+        with open(file_path, "r") as f:
+            print(f.read())
+        print(file)
+
 
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
